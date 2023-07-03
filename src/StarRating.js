fbc491609a40d72ec42edbdb6f7608ffe12c4579
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-03 19:54:38 +0530

    done with the starRating Component

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { useState } from "react";
 
-const starStyle = {
-    width: "48px",
-    height: "48px",
-    display: "block",
-    cursor: " point",
-};
 const containerStyle = {
     display: "flex",
     alignItems: "center",
@@ -24,6 +18,7 @@ export const StarRating = ({
     size = 50,
     color = "#fcc419",
     starMessages = ["Awful", "Meh", "Decent", "Great", "Awesome"],
+    setuserMovieRating,
 }) => {
     const textStyle = {
         lineHeight: "1",
@@ -47,6 +42,8 @@ export const StarRating = ({
                         key={i}
                         rateChangeHandler={() => {
                             rateChangeHandler(i + 1);
+                            setuserMovieRating(i + 1);
+                            console.log("userRating changed");
                         }}
                         full={rating - 1 >= i}
                     />
@@ -98,26 +95,3 @@ const Star = ({ rateChangeHandler, full, color, size }) => {
     );
 };
 
-/*
-FULL STAR
-
-
-
-
-EMPTY STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg>
-
-*/
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { StarRating } from "./StarRating";
+import { useState } from "react";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const Test = () => {
+    const [userMovieRating, setuserMovieRating] = useState(null);
+
+    return (
+        <div>
+            the movies is rated {userMovieRating}
+            <StarRating
+                maxRating={"efadwad"}
+                size={30}
+                defaultRating={3}
+                color={"#eab308"}
+                starMessages={["Awful", "Meh", "Decent", "Great", "Awesome"]}
+                setuserMovieRating={setuserMovieRating}
+            />
+        </div>
+    );
+};
+
 root.render(
-  <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      size={30}
-      defaultRating = {3}
-      color={"#eab308"}
-      starMessages={["Awful", "Meh", "Decent", "Great", "Awesome"]}
-    />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Test />
+    </React.StrictMode>
 );
