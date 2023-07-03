de0fe0937ade65922054f93bf991037fc7df46af
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-03 13:11:12 +0530

    added the ful star icon svg to the star component and defined some styles

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <StarRating />
+        <StarRating maxRating={10}/>
+        <StarRating maxRating={5}/>
     </React.StrictMode>
 );
