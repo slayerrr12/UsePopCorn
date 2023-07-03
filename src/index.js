0ab4f070e2763bcb5f873765958935fdb2f78509
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-03 15:57:28 +0530

    made the starRating component more usable by adding moe props and now the developers wil have more authority and control over it you can customize the color size maxvalue accordingly thorugh props

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <StarRating maxRating={10}/>
-        
+        <StarRating maxRating={10} size={30} color={"#eab308"} />
+
     </React.StrictMode>
 );
