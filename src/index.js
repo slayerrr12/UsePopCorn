9b74f69c73a233493b3f6ed72942d18619e4b66a
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-03 14:45:58 +0530

    added a onclick effect on the starComponenet and it works fine

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <StarRating maxRating={10}/>
-        <StarRating maxRating={5}/>
+        
     </React.StrictMode>
 );
