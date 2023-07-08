f95b348babdcbc4904536825a12b57f63787e753
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-08 20:20:51 +0530

    added an omdb third party api to fetch movies and also added a loader component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import { useState } from "react";
+import "./index.css";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
+import { useEffect, useState } from "react";
 
 const tempWatchedData = [
   {
@@ -51,19 +29,48 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [watched, setWatched] = useState(tempWatchedData);
+  const onChangeQuery = (e) => {
+    e.preventDefault();
+    const queryValue = e.target.value;
+    setQuery(queryValue);
+  };
+
+  //side effect code
+  useEffect(() => {
+    
+    async function fetchMoviesData() {
+      setLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?s=${query.trim().replace(/\s+/g, " ")}&apikey=a8902e28`
+        );
+
+        const data = await res.json();
+        console.log("data from api:", data);
+        
+        setMovies(data.Search);
+        setLoading(false);
+      
+        
+      }
+      fetchMoviesData()
+      
+    
+  }, [query]);
 
   return (
     <>
       <NavBar>
-        <Search />
+        <Search onChangeQuery={onChangeQuery} query={query} />
         <NumResults movies={movies} />
       </NavBar>
 
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {loading ? <LoadingSpinner /> : <MovieList movies={movies} />}
         </Box>
 
         <Box>
@@ -72,6 +79,15 @@ export default function App() {
         </Box>
       </Main>
     </>
+  );
+}
+
+function LoadingSpinner() {
+  console.log("loading");
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
   );
 }
 
@@ -93,21 +109,24 @@ function Logo() {
   );
 }
 
-function Search() {
-  const [query, setQuery] = useState("");
-
+function Search({ onChangeQuery, query }) {
+ 
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
-      onChange={(e) => setQuery(e.target.value)}
+      onChange={onChangeQuery}
     />
   );
 }
 
 function NumResults({ movies }) {
+  if (!movies) {
+    return null; // or you can display a loading indicator
+  }
+
   return (
     <p className="num-results">
       Found <strong>{movies.length}</strong> results
@@ -132,31 +151,6 @@ function Box({ children }) {
     </div>
   );
 }
-
-/*
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </>
-      )}
-    </div>
-  );
-}
-*/
 
 function MovieList({ movies }) {
   return (
